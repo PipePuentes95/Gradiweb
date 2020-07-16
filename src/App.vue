71ad9532ca8aba5c88c3979bf89322790eeab607
David <dafelpu.farfan95@gmail.com>
2020-07-15 22:44:53 -0500

    project preparation & API call

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      data: {}
+    }
+  },
+  beforeCreate() {
+    const consultAPI = () => {
+      const APIKEY = "acbe571709d34e4160a6ece108b21586";
+      // const url = `http://api.openweathermap.org/data/2.5/weather?q=bogota,colombia&appid=${APIKEY}`;
+      // Este trae el del dia presente y 7 dias en adelante
+      const url = `https://api.openweathermap.org/data/2.5/onecall?lat=4.60971&lon=-74.08175&exclude=minutely,hourly,&appid=${APIKEY}`;
+
+      let req =  new XMLHttpRequest();
+      req.open("GET", url, true);
+      
+        console.log(req);
+      req.onreadystatechange = () => {
+        req.status === 200 ? this.data = req.response  : console.log('Error');
+      }
+      req.send(null);
+    }
+    consultAPI();
+  },
+  computed: {
+    formatData() {
+      return JSON.parse(this.data)
+    }
+    
   }
+  
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
